d88b94133b5080867c9a1e36ce6501d5a47043e7
ahodzic2 <72961430+ahodzic2@users.noreply.github.com>
2022-04-04 17:35:35 -0500

    main setup and stage 2 visualization
    main structure established and visualization for stage 2.

--- Stage2.py
+++ Stage2.py
@@ -1,6 +1,11 @@
 from github import Github
 import csv
 import os
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.dates as mdates
+from datetime import datetime
+import calendar
 
 def importStageTwo(token, strRepo):
     g = Github(token)
@@ -8,7 +13,7 @@ def importStageTwo(token, strRepo):
 
     file = 'data2.csv'
     #Delete file if it exists
-    if(os.path.exists(file) and os.path.isfile(file)):
+    if os.path.exists(file) and os.path.isfile(file):
         os.remove(file)
 
     csvFile = open(file, 'w+', encoding='utf-8')
@@ -35,4 +40,37 @@ def importStageTwo(token, strRepo):
     csvFile.close()
 
 def visualizeStageTwo():
-    print('TBD')
+    read_file = open('data2.csv')
+    csvReader = csv.DictReader(read_file)
+
+    daysOfWeek = {
+        "Sunday": 0,
+        "Monday": 0,
+        "Tuesday": 0,
+        "Wednesday": 0,
+        "Thursday": 0,
+        "Friday": 0,
+        "Saturday": 0
+    }
+
+    # Calculate number of commits by day of week from data file,
+    for row in csvReader:
+            dayofWeek = calendar.day_name[datetime.strptime(row["date"], "%Y-%m-%d %H:%M:%S").weekday()]
+            daysOfWeek[str(dayofWeek)] += 1
+
+    # Convert dictionary key/value into independent lists
+    days = list(daysOfWeek.keys())
+    daySum = list(daysOfWeek.values())
+
+    # Plot Data
+    plt.figure(figsize=(12.5, 10))
+    plt.barh(days, daySum)
+    plt.title('Commits by Weekday')
+    plt.xlabel('Number of Commits')
+    plt.ylabel('Day of Week')
+    # Bar Values
+    for index, value in enumerate(daySum):
+        plt.text(value, index,
+                 str(value))
+    plt.savefig("commitTimeline.png")
+    plt.show()
--- main.py
+++ main.py
@@ -1,14 +1,43 @@
-from xml.etree.ElementTree import Comment
-
+import sys
 import Stage1
 import Stage2
 import Stage3
 import Stage4
 
-GITHUB_TOKEN = '' #ENTER TOKEN HERE
+GITHUB_TOKEN = ''  # ENTER TOKEN HERE
 REPO = "freeCodeCamp/freeCodeCamp"
 
 if __name__ == '__main__':
-    Stage1.importStageOne(GITHUB_TOKEN, REPO)
-    Stage2.importStageTwo(GITHUB_TOKEN, REPO)
-    Stage3.importStageThree(GITHUB_TOKEN, REPO)
+    stageInput = -1
+    importInput = -1
+
+    while stageInput != 0 and stageInput != 1 and stageInput != 2 and stageInput != 3 and stageInput != 4:
+        if stageInput == 0:
+            sys.exit("User Exit")
+        stageInput = int(input("Which stage would you like to run (0 = Exit, 1, 2, 3, 4): "))
+
+    while importInput != 0 and importInput != 1 and importInput != 2:
+        if importInput == 0:
+            sys.exit("User Exit")
+        importInput = int(input("Would you like to run the import process? (0 = Exit, 1 = Yes, 2 = No): "))
+
+    if stageInput == 1:
+        if importInput == 1:
+            Stage1.importStageOne(GITHUB_TOKEN, REPO)
+
+        Stage1.visualizeStageOne()
+    elif stageInput == 2:
+        if importInput == 1:
+            Stage2.importStageTwo(GITHUB_TOKEN, REPO)
+
+        Stage2.visualizeStageTwo()
+    elif stageInput == 3:
+        if importInput == 1:
+            print('TODO')
+
+        print('TODO')
+    elif stageInput == 4:
+        if importInput == 1:
+            print('TODO')
+
+        print('TODO')
